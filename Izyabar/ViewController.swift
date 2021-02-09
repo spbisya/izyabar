0fa7d0a9cf6a793f0dfe986787689da34b2fb88b
Izya Pitersky <wtcute.info@gmail.com>
2021-02-10 00:41:20 +0300

    Initial Commit

---
+++ Izyabar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Izyabar
+//
+//  Created by Izya Pitersky on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
